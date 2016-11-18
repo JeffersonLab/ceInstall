dc74ece2cc4d0fbf63abd8310ca361aed61d29f0
Brad Sawatzky <brads@jlab.org>
2016-11-18 09:27:47 -0500

    Add a production.sh setup script; update jlab.sh wrapper
    - These scripts are wrappers that end up calling the .csh scripts.  They
      should be able to remain static across releases since the csh scripts
      do all the heavy lifting.
    
    - The production.sh script invokes the jlab.sh script and follows the
      syntax and convention of the production.csh wrapper.
    
      The 'production.sh' script should go in /site/12gev_phys/
         production.sh  --> /site/12gev_phys/production.sh
      I would drop 'jlab.sh' into /site/12gev_phys/ too and then symlink
      into the <ver>/ce/ directories:
             jlab.sh        --> /site/12gev_phys/jlab.sh
      Make the symlinks:
      % cd /site/12gev_phys/
      % for v in 1.* 2.0 devel; do ln -s ../../jlab.sh $v/ce/jlab.sh; done
    
      For now I've put the jlab.sh script into the 2.0/ce directory as
      requested.
    
    - The rewritten jlab.sh script attempts to resolve the following issues:
    
      - The old script tests for and requires JLAB_ROOT and JLAB_VERSION to
        be set despite being explicitly sourced from a pathname that is
        normally derived from those same variables.  So those variables are
        either redundant if the invocation path matches, or will cause
        problems if there is a mismatch (since daughter scripts will honor
        the incorrect JLAB_* variables).
    
      - The version list in the old jlab.sh preamble is out of date (and
        those checks should be handled within the 'real' tcsh setup script
        anyway, not in this wrapper.)
    
      - I see what it is trying to do with the (scary[*]) inlined invocation
        of the tcsh jlab.csh setup script, followed by parsing 'printenv'
        and exporting back into the bash environment, but there are a number
        of issues:
        - The 'sed s/ //g' call globally strips spaces from the printenv
          lines but then resets the same variable, which screws up any bash
          variable arguments that contained spaces.
    
        - There seems to be confusion within the jlab.sh script where the
          assignment op '=' is used when it should be the comparison op
          '=='.
    
        - It explicitly reprints the module version information, duplicating
          a bunch of code in jlab.csh.
    
      [*] The inline tcsh invocation approach still freaks me out, but it
          does seem to work...

--- 2.0/ce/jlab.sh
+++ 2.0/ce/jlab.sh
@@ -1,131 +1,53 @@
 #
 # You must source this script. At your bash prompt type: ". <path>/jlab.sh"
 #
-# Get the JLAB environment variables by parsing the script in tcsh and then printing the  environment. 
-# We need to filter out the spaces, since that confuses the for loop. We also need to drop some
-# tcsh specific environment variables from the export list.
+# Get the JLAB environment variables by parsing the script in tcsh and then
+# printing the  environment.  We need to filter out the spaces, since that
+# confuses the for loop. We also need to drop some tcsh specific environment
+# variables from the export list.
 #
 # Author: Maurik Holtop, maurik@physics.unh.edu
 # Date:   August, 2016
 #
-if [ -z $JLAB_ROOT ]; then
-    echo "Please set JLAB_ROOT first."
-    return
-fi
-
-while [ -z $JLAB_VERSION ]; do
-    echo "Please choose a version:"
-    select vers in 1.1 1.2 1.3 1.3.1 devel; do
-       export JLAB_VERSION=${vers}
-       break
-    done
-    if [ ! -d ${JLAB_ROOT}/${JLAB_VERSION} ]; then
-	echo "Directory ${JLAB_ROOT}/${JLAB_VERSION} does not exist, please set JLAB_ROOT correctly. "
-	unset JLAB_VERSION
-	unset JLAB_ROOT
-	return
-    fi
-
-done
+# - Brad Sawatky <brads@jlab.org>  (Nov 17, 2016)
+#   - rewrote script to fix a few issues
 
 
+TMPF=`mktemp`
+tcsh ${TCSH_ARG} -c "source ${JLAB_ROOT}/${JLAB_VERSION}/ce/jlab.csh ${JLAB_VERSION}; printenv" | perl -e '
+  my %IGNORE;
+  ## Add variables to ignore to the space-delimited list below
+  foreach( qw(
+    HISTTIMEFORMAT SHELL PS1 overwrite _ BASH_FUNC_module\(\)
+  )){ $IGNORE{$_}="yes"; }
 
-for i in `tcsh -c "source ${JLAB_ROOT}/${JLAB_VERSION}/ce/jlab.csh ${JLAB_VERSION}; printenv" | grep "=" |  sed 's/ //g' `;  do 
-    var=$(echo $i | cut -d= -f 1)
-#
-# Filter out some variables.
-#
-    if [ "$var" = "HISTTIMEFORMAT" ];     then continue; fi
-    if [ "$var" = "SHELL" ];              then continue; fi
-    if [ "$var" = "PS1" ];                then continue; fi
-    if [ "$var" = "overwrite" ];          then continue; fi
-    if [ "$var" = "_" ];                  then continue; fi
-    if [ "$var" = "BASH_FUNC_module()" ]; then continue; fi
+  while(<>) {
+    chomp;
 
- # ON Mac, the DYLD_LIBRARY_PATH is not passed to tcsh, so preserve explicitly.
-    if   [ "${OSTYPE}" = "darwin" ] && [ "$var" = "LD_LIBRARY_PATH" ];  then 
-	export ${i%:}:${LD_LIBRARY_PATH}  # Remove trailing : if it is there.
-    elif [ "${OSTYPE}" = "darwin" ] && [ "$var" = "DYLD_LIBRARY_PATH" ]; then 
-	export ${i%:}:${DYLD_LIBRARY_PATH}
-    else
-	export ${i}
-    fi
-done
+    ## Look for and treat variables from printenv here
+    if(/(.*?)=(.*)/) {
+      my $var = $1;
+      my $arg = $2;
 
-if [ ! -z $PS1 ]; then 
-    echo
-    echo " > Common Environment Version: <"$JLAB_VERSION">  "$CE_DATE
-    echo " > Running as "`whoami` on `hostname`
-    echo " > OS Release:    "$OSRELEASE
-    echo " > JLAB_ROOT set to:     "$JLAB_ROOT
-    echo " > JLAB_SOFTWARE set to: "$JLAB_SOFTWARE
-    echo 
-    if [ ! -d $CLHEP_BASE_DIR/lib ]; then
-	echo " ? Attention: No CLHEP installation found for version ${CLHEP_VERSION}"
-    else
-	echo " > CLHEP    version: ${CLHEP_VERSION} "
-    fi
-    if [ ! -d $QTDIR/lib ]; then
-	echo " ? Attention: No QT   installation found for version ${QT_VERSION}"
-    else
-	echo " > QT       version: ${QT_VERSION} "
-    fi
-    if [ ! -d ${XERCESCROOT}/lib ]; then
-	echo " ? Attention: No XERCESC installation found for version ${XERCESC_VERSION}"
-    else
-	echo " > XERCES    version:  ${XERCESC_VERSION}"
-    fi
-    if [ ! -d ${MYSQLIB} ]; then
-	echo " ? Attention: No MYSQL installation found."
-    else
-	echo " > MYSQL     version from:  ${MYSQLIB}"
-    fi
-    if [ ! -d $G4INSTALL/bin ]; then
-	echo " ? Attention: No GEANT4 installation found for version ${G4VERSION}"
-    else
-	echo " > GEANT4   version: ${G4VERSION} "
-    fi
-    if [ ! -d $ROOTSYS/lib ]; then
-	echo " ? Attention: No ROOT installation found for version ${ROOT_VERSION}"
-    else
-	echo " > ROOT     version: ${ROOT_VERSION} "
-    fi
-    if [ ! -d $MLIBRARY ]; then
-	echo " ? Attention: No MLIBRAY installation found for version ${MLIBRARY_VERSION}"
-    else
-	echo " > MLIBRARY    version: ${MLIBRARY_VERSION} "
-    fi
-    if [ ! -d $GEMC ]; then
-	echo " ? Attention: No GEMC installation found."
-    elif [ ! -z $GEMC_VERSION ]; then
-	echo " > GEMC     version: "$GEMC_VERSION
-    else
-	echo " > Custom GEMC : "$GEMC
-    fi
-    if [ ! -d $JANA_HOME ]; then
-	echo " ? Attention: No JANA installation found."
-    else
-	echo " > JANA     version: "$JANA_VERSION
-    fi
-    if [ ! -d $JLAB_ROOT/$JLAB_VERSION/scons_bm/$SCONS_BM_VERSION ]; then
-	echo " ? Attention: No Scons Script installation found."
-    else
-	echo " > S Build  version: "$SCONS_BM_VERSION
-    fi
-    if [ ! -d $EVIO/lib ]; then
-	echo " ? Attention: No EVIO installation found."
-    else
-	echo " > EVIO     version: "$EVIO_VERSION
-    fi
-    if [ ! -d $CCDB_HOME/lib ]; then
-	echo " ? Attention: No CCDB installation found."
-    else
-	echo " > CCDB     version: "$CCDB_VERSION
-    fi
-    if [ ! -d $BANKS/lib ]; then
-	echo " ? Attention: No BANKS installation found."
-    else
-	echo " > Banks    version: "$BANKS_VERSION
-    fi
+      next if( exists($IGNORE{$var}) );
 
-fi
+      if( $ENV{OSTYPE} eq "darwin" ) {
+        if( $var =~ /(DY)?LD_LIBRARY_PATH/ ) {
+          $arg =~ s/:$//;   # strip trailing colon
+          print "export ${var}=\"${arg}:$ENV{$var}\"\n";
+        }
+        next;
+      }
+
+      ## DEFAULT is to re-export the variable list into the bash environment
+      print "export ${var}=\"${arg}\"\n";
+      next;
+    }
+
+    ## Dump information messages (and everything else) to STDERR where it should not hurt anyone
+    print STDERR "$_\n";
+  }
+' > $TMPF
+
+source "$TMPF"
+rm -f "$TMPF"
