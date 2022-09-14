a9e13d1327e5998c38a6279985b4afc8198a7ef8
Maurizio Ungaro <ungaro@jlab.org>
2022-09-14 09:51:26 -0400

    osrelease to include only major os and major compiler version

--- ce/osrelease.py
+++ ce/osrelease.py
@@ -3,52 +3,52 @@
 import os
 import platform
 
+thisPlatform = platform.system()
 osreleasVersion = ''
 compilerVersion = ''
 
-# Darwin
-if platform.system() == 'Darwin':
-    # Getting macos version
-    # platform.mac_ver() returns:
-    # ('10.15.3', ('', '', ''), 'x86_64')
-    version, dummy1, dummy2 = platform.mac_ver()
-    # setting the maxsplit parameter to 1, will return a list with 2 elements
-    major, minor = version.split('.', 1)
-    osreleasVersion = 'macosx' + major + '.' + minor
+# Darwin: clang
+if thisPlatform == 'Darwin':
+	# Getting macos version
+	# platform.mac_ver() returns:
+	# ('10.15.3', ('', '', ''), 'x86_64')
+	version, dummy1, dummy2 = platform.mac_ver()
+	# setting the maxsplit parameter to 1, will return a list with 2 elements
+	major, minor = version.split('.', 1)
+	osreleasVersion = 'macosx' + major
+	
+	# Getting clang version
+	# This assumes a return from clang --version first line like this one:
+	# Apple clang version 14.0.0 (clang-1400.0.29.102)
+	clangVersion = os.popen('clang --version').readlines()[0].split()[3]
+	
+	compilerVersion = 'clang' + clangVersion.split('.')[0]
 
-    # Getting clang version
-    # This assumes a return from clang like this:
-    #
-    # Apple clang version 11.0.0 (clang-1100.0.33.17)
-    # Target: x86_64-apple-darwin19.3.0
-    # Thread model: posix
-    # InstalledDir: /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin
-
-    clangVersion = 'clang --version'
-    stream = os.popen(clangVersion)
-    output = stream.readlines()
-    clangVersions = output[0].split(' ')
-    compilerVersion = 'clang' + clangVersions[3]
-
-elif platform.system() == 'Linux':
-
-    if os.path.exists('/etc/redhat-release'):
-        with open('/etc/redhat-release') as f:
-          columns = f.read().strip().split()
-          if columns[0] == 'Fedora':
-            osreleasVersion = columns[0]+columns[2]
-          else:
-            osreleasVersion = columns[0]+columns[3]
-    else:
-        raise ValueError('Unsupported Linux Version')
-
-    compilerVersion = os.popen('gcc --version').readlines()
-    compilerVersion = 'gcc'+compilerVersion[0].split()[2]
+# Linux: gcc
+elif thisPlatform == 'Linux':
+	
+	# fedora line
+	if os.path.exists('/etc/redhat-release'):
+		with open('/etc/redhat-release') as f:
+			columns = f.read().strip().split()
+			if columns[0] == 'Fedora':
+				osreleasVersion = columns[0]+columns[2].split('.')[0]
+			else:
+				osreleasVersion = columns[0]+columns[3].split('.')[0]
+	else:
+		raise ValueError('Unsupported Linux Version: no /etc/redhat-release')
+	
+	# Getting clang version
+	# This assumes a return from gcc --version first line like this one:
+	# gcc (GCC) 4.8.5 20150623 (Red Hat 4.8.5-44)
+	gccVersion = os.popen('gcc --version').readlines()[0].split()[2]
+	
+	compilerVersion = 'gcc' + gccVersion.split('.')[0]
 
 else:
-    raise ValueError(('Unsupported platform: '+platform.system()))
+	raise ValueError(('Unsupported platform: ' + thisPlatform ))
 
-osname = platform.system() + '_' + osreleasVersion
+osname = osreleasVersion
 osname += '-' + compilerVersion
 
 print (osname)
