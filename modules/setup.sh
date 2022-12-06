674362eeab4364a4c1f7c03b72d02a5f863fdcda
maureeungaro <maureeungaro@gmail.com>
2022-12-06 10:50:04 -0500

    removed sourcing of /etc/profile.d/modules.sh

--- modules/setup.sh
+++ modules/setup.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-# load module for the current OS
-case $(uname -s) in
-    Linux)
-        . /etc/profile.d/modules.sh
-        ;;
-    Darwin)
-        brewDir=$(brew --prefix)
-        . "$brewDir"/opt/modules/init/sh
-        ;;
-    *)
-        echo "Unsupported OS: $(uname -s)"
-        exit 1
-        ;;
-esac
 
 # extract path to this file
 export modules_home=$(dirname "$(realpath "${BASH_SOURCE:-$0}")")
