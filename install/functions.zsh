2984750fd48f45598e601faa292ac0787ca473cc
maureeungaro <maureeungaro@gmail.com>
2023-02-21 17:08:40 -0500

    just reorder functions

--- install/functions.zsh
+++ install/functions.zsh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
-yellow=`tput setaf 3`
-blue=`tput setaf 4`
-red=`tput setaf 1`
-green=`tput setaf 2`
+red=`    tput setaf 1`
+green=`  tput setaf 2`
+yellow=` tput setaf 3`
+blue=`   tput setaf 4`
 magenta=`tput setaf 5`
-reset=`tput sgr0`
+reset=`  tput sgr0`
 
 # needed otherwise cmake could pick up the system cc
 export CC=gcc
