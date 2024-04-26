9ff600028e0140c6aa7fd6bb2ef7c0fd401a29b4
mungaro <maurizio.ungaro@cnu.edu>
2024-04-26 15:34:30 -0400

    gemc3 v1.2 and meson install

--- install/functions.zsh
+++ install/functions.zsh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
-red=`    tput setaf 1`
-green=`  tput setaf 2`
-yellow=` tput setaf 3`
-blue=`   tput setaf 4`
-magenta=`tput setaf 5`
-reset=`  tput sgr0`
+red=$(   tput setaf 1)
+green=$( tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(  tput setaf 4)
+magenta=$(tput setaf 5)
+reset=$( tput sgr0)
 
 # needed otherwise cmake could pick up the system cc
 export CC=gcc
@@ -16,10 +16,9 @@ repo="https://www.jlab.org/12gev_phys/packages/sources"
 
 n_cpu=$(getconf _NPROCESSORS_ONLN)
 
-
 whine_and_quit() {
-  echo "$red $1 error $reset"
-  exit 1
+	echo "$red $1 error $reset"
+	exit 1
 }
 
 log_general() {
@@ -49,7 +48,7 @@ unpack_source_in_directory_from_jlab_repo() {
 	dir=$2
 	tar_strip=$3
 	filename=$(basename "$url")
-	
+
 	echo
 	echo " > unpack_source_in_directory_from_jlab_repo: "
 	echo " > url: $url"
@@ -57,17 +56,17 @@ unpack_source_in_directory_from_jlab_repo() {
 	echo " > tar_strip: $tar_strip"
 	echo " > filename: $filename"
 	echo
-	
+
 	dir_remove_and_create "$dir"
 	cd "$dir" || whine_and_quit "cd $dir"
-	
+
 	echo "$magenta > Fetching source from $url onto $filename$reset"
 	rm -f "$filename"
-	curl -sSL  "$url" --output "$filename"
-	
+	curl -sSL "$url" --output "$filename"
+
 	echo "$magenta > Unpacking $filename in $dir$reset"
 	echo
-	
+
 	tar -zxpf "$filename" --strip-components="$tar_strip"
 	rm -f "$filename"
 }
@@ -78,10 +77,10 @@ unpack_source_in_directory_from_url() {
 	tar_strip=$3
 	# if the fourth argument 'no_remove' is given, do not remove the tarball
 	if [ "$#" -eq 4 ]; then
-    no_remove=$4
-  else
-    no_remove=0
-  fi
+		no_remove=$4
+	else
+		no_remove=0
+	fi
 
 	filename=$(basename "$url")
 
@@ -93,19 +92,19 @@ unpack_source_in_directory_from_url() {
 	echo " > filename: $filename"
 	echo
 
-  # if no_remove is 1, do not remove the tarball
-  if [ "$no_remove" -eq 0 ]; then
-    dir_remove_and_create "$dir"
-  else
-    mkdir -p "$dir"
-  fi
+	# if no_remove is 1, do not remove the tarball
+	if [ "$no_remove" -eq 0 ]; then
+		dir_remove_and_create "$dir"
+	else
+		mkdir -p "$dir"
+	fi
 
 	cd "$dir" || whine_and_quit "cd $dir"
 
 	echo "$magenta > Fetching source from $url onto $filename$reset"
 	rm -f "$filename"
-#	wget "$url" || whine_and_quit "wget $url"
-	curl  -S --location-trusted --progress-bar  --retry 4 "$url" -O || whine_and_quit "wget $url"
+	#	wget "$url" || whine_and_quit "wget $url"
+	curl -S --location-trusted --progress-bar --retry 4 "$url" -O || whine_and_quit "wget $url"
 	echo "Downloaded file:"
 	ls -lrt $filename
 
@@ -116,6 +115,29 @@ unpack_source_in_directory_from_url() {
 	rm -f "$filename"
 }
 
+clone_tag() {
+	url=$1
+	version=$2
+	source_dir=$3
+
+	echo
+	echo " > url: $url"
+	echo " > clone_tag: " $version
+	echo " > in directory: $install_dir"
+
+	git clone --recurse-submodules --single-branch -b $version "$url" "$source_dir"
+	echo
+}
+
+meson_install() {
+	meson_options=$1
+	install_dir=$2
+
+	meson setup build "$meson_options" --wipe
+	cd build
+	meson configure -Dprefix="$install_dir"
+	meson install
+}
 
 cmake_build_and_install() {
 	source_dir=$1
@@ -123,35 +145,35 @@ cmake_build_and_install() {
 	install_dir=$3
 	cmake_options=$4
 	do_not_delete_source=$5
-	
+
 	echo " > source_dir:    $source_dir"
 	echo " > build_dir:     $build_dir"
 	echo " > install_dir:   $install_dir"
 	echo " > cmake_options: $cmake_options"
-	
+
 	local cmd_start="$SECONDS"
 
-  # install_dir is the base directory containing $build_dir
+	# install_dir is the base directory containing $build_dir
 	dir_remove_and_create "$build_dir"
 	cd "$build_dir" || whine_and_quit "cd $build_dir"
-	
+
 	echo
 	echo "$magenta > Configuring cmake with: $=cmake_options$reset"
 	echo
-	
-	cmake  "$source_dir" -DCMAKE_INSTALL_PREFIX="$install_dir" $=cmake_options  2>"$install_dir/cmake_err.txt" 1>"$install_dir/cmake_log.txt" || whine_and_quit "cmake $source_dir -DCMAKE_INSTALL_PREFIX=$install_dir $=cmake_options"
-	
+
+	cmake "$source_dir" -DCMAKE_INSTALL_PREFIX="$install_dir" $=cmake_options 2>"$install_dir/cmake_err.txt" 1>"$install_dir/cmake_log.txt" || whine_and_quit "cmake $source_dir -DCMAKE_INSTALL_PREFIX=$install_dir $=cmake_options"
+
 	echo "$magenta > Building$reset"
 	make -j "$n_cpu" 2>$install_dir/build_err.txt 1>"$install_dir/build_log.txt" || whine_and_quit "make -j $n_cpu"
-	
+
 	echo "$magenta > Installing$reset"
-	make install  2>$install_dir/install_err.txt 1>"$install_dir/install_log.txt" || whine_and_quit "make install"
+	make install 2>$install_dir/install_err.txt 1>"$install_dir/install_log.txt" || whine_and_quit "make install"
 
 	echo " Content of $install_dir:"
 	ls -l "$install_dir"
 	if [[ -d "$install_dir/lib" ]]; then
-	    echo " Content of $install_dir/lib:"
-	    ls -l "$install_dir/lib"
+		echo " Content of $install_dir/lib:"
+		ls -l "$install_dir/lib"
 	fi
 
 	# cleanup
@@ -159,15 +181,14 @@ cmake_build_and_install() {
 	rm -rf "$build_dir"
 
 	# if delete_source is set, do not delete the source directory
-  if [ -z "$do_not_delete_source" ]; then
-    echo "$magenta > Deleting source directory...$reset"
-    rm -rf "$source_dir"
-  fi
-
+	if [ -z "$do_not_delete_source" ]; then
+		echo "$magenta > Deleting source directory...$reset"
+		rm -rf "$source_dir"
+	fi
 
 	local cmd_end="$SECONDS"
-	elapsed=$((cmd_end-cmd_start))
-	
+	elapsed=$((cmd_end - cmd_start))
+
 	echo "$magenta > Compilation and installation completed in $elapsed seconds.$reset"
 }
 
@@ -178,33 +199,33 @@ scons_build_and_install() {
 	build_shared=''
 	# if a second argument is given to this function, set build_shared to it
 	if [ -n "$2" ]; then
-        build_shared=$2
-        echo " > build_shared:  $build_shared"
-    fi
+		build_shared=$2
+		echo " > build_shared:  $build_shared"
+	fi
 
 	echo " > install_dir:   $install_dir"
-	
+
 	local cmd_start="$SECONDS"
 	cd "$install_dir" || whine_and_quit "cd $install_dir"
 
 	scons_options=" -j$n_cpu OPT=1 $build_shared"
 	echo "$magenta > Building using scons options: > $=scons_options < $reset"
 	rm -f .sconsign.dblite # for some reason this still linger
-#	\scons "$scons_options" 2>"$install_dir/build_err.txt" 1>"$install_dir/build_log.txt" || whine_and_quit "\scons $scons_options"
+	#	\scons "$scons_options" 2>"$install_dir/build_err.txt" 1>"$install_dir/build_log.txt" || whine_and_quit "\scons $scons_options"
 	\scons "$=scons_options"
 
 	# cleanup
 	echo "$magenta > Cleaning up...$reset"
 	find ./ -name "*.o" -delete
 	local cmd_end="$SECONDS"
-	elapsed=$((cmd_end-cmd_start))
+	elapsed=$((cmd_end - cmd_start))
 
 	echo " Content of this dir:"
-    ls -l
+	ls -l
 
 	if [[ -d "lib" ]]; then
-	    echo " Content of lib:"
-	    ls -l lib/
+		echo " Content of lib:"
+		ls -l lib/
 	fi
 
 	echo "$magenta > Compilation and installation completed in $elapsed seconds.$reset"
@@ -215,6 +236,6 @@ function moduleTestResult() {
 	version=$2
 
 	# we're only interested in the exit status
-	module test "$library/$version" &> /dev/null
+	module test "$library/$version" &>/dev/null
 	echo $?
 }
